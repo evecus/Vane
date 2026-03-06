c22a942fe1dbe71ac867e9c1bfcf4624264f0416
evecus <jamie55@163.com>
2026-03-06 21:40:44 +0800

    Update main.js

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import VueApexCharts from 'vue-apexcharts'
+import VueApexCharts from 'vue3-apexcharts'
 import router from './router'
 import App from './App.vue'
 import './style.css'
